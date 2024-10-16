0e5f409ad5e05b8c45b5f9efa8da6cdf16ded0f1
runningpoem30 <aryaanandpathak30@gmail.com>
2024-10-16 11:48:43 +0530

    .

--- backend/smart-wallet-backend/server.js
+++ backend/smart-wallet-backend/server.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const app = express();
 const PORT =  3000;
+const connectDB =  require('./config/database')
 
 
-app.get('/' , (req , res ) => {
-  res.send("hi there server is listening")
-})
+app.use(express.json());
 
 
-
-
+//routes
+app.use('/api/auth' , require('./routes/authRoutes'))
+app.use('/api/wallet' , require('./routes/walletRoutes'))
+app.use('/apo/points' , require('./routes/pointsRoutes'));
+app.use('/api/transaction' , require('./routes/transactionRoutes'))
 
 
 console.log("app is listening")
